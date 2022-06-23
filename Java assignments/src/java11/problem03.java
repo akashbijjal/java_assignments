80a83c36cdf442b7a2236336a4b59b7427d8e6a8
akashnbijjal <akash.bijjal@gmail.com>
2022-06-23 19:37:45 +0530

    java 11

---
+++ Java assignments/src/java11/problem03.java
@@ -0,0 +1,20 @@
+package java11;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class problem03 {
+public static void main(String[] args) {
+	ArrayList<String> list=new ArrayList();
+	list.add("A quick brown fox jumps over the lazy dog");
+	System.out.println(list);
+	
+	
+	System.out.println("******************");
+	System.out.println("converted to Array");
+	String [] arr=list.toArray(String[]::new);
+	System.out.println(Arrays.toString(arr));
+	
+	
+}
+}
